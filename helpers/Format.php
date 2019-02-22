ef4cf8c6f35759bb89836ac1cf8a42395044f5b3
Imtiaze <ahammedimtiaze78@gmail.com>
2019-02-23 00:32:35 +0600

    dummy post created on the database. all post dynamically show in the index page. date format nicely design, post details up to 400 words

---
+++ helpers/Format.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Format {
+
+  public function formatDate($date) {
+    return date('F j, Y, D g:i a', strtotime($date));
+  }
+
+  public function formatPost($postBody, $limit=400) {
+    $postBody = $postBody." ";
+    $postBody = substr($postBody, 0, $limit);
+    $postBody = substr($postBody, 0, strrpos($postBody, ' '));
+    $postBody = $postBody."...";
+    return $postBody;
+  }
+
+
+
+}
